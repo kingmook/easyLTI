34d7114b945c9abdd6e2b8ee28139df80f643797
Michael Brousseau <mbrousseau@ctlet.brocku.ca>
2015-07-22 11:48:44 -0400

    Initial Commit

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+/* Definition of required variables for the LTI tool */
+/* Change as appropriate for your configuration */
+/* MBrousseau July 2015 */
+
+//LTI Information
+$GLOBALS['ltiSecret'] = 'Secret';
+$GLOBALS['ltiKey'] = 'Key';
+
+//Database to connect to at minimum for storing nonces
+$GLOBALS['dbHost'] = 'localhost';
+$GLOBALS['dbName'] = 'database';
+$GLOBALS['dbUser'] = 'username';
+$GLOBALS['dbPass'] = 'password';
+
